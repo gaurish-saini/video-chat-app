eaf2f425940ae07d6308c2c67151ecb2186f7c28
Gaurish Prakhar Saini <gaurishprakhar@gmail.com>
2022-03-03 06:15:37 +0530

    accessing data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
     <h1>Home</h1>
+    <p>Welcome {{ user }}</p>
   </div>
 </template>
+
+<script>
+import db from '../db.js'
+
+export default {
+  name: 'homeView',
+  data: function () {
+    return {
+      user: null
+    }
+  },
+  mounted() {
+    db.collection('users')
+      .doc('atNe27NkX1Ij80jJAfzX')
+      .get()
+      .then(snapshot => {
+        this.user = snapshot.data().name
+      })
+  }
+}
+</script>
