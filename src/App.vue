6567a6666e96194c574b2d9d1be334a755d6acc7
Gaurish Prakhar Saini <gaurishprakhar@gmail.com>
2022-03-03 06:53:53 +0530

    data access to all components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
-  <NavigationBar />
-  <router-view />
+  <NavigationBar :user="user" />
+  <router-view :user="user" />
 </template>
 
 <script>
+import db from './db.js'
 import NavigationBar from '@/components/NavigationBar'
-
 export default {
+  name: 'AppView',
+  data: function () {
+    return {
+      user: null
+    }
+  },
+  mounted() {
+    db.collection('users')
+      .doc('atNe27NkX1Ij80jJAfzX')
+      .get()
+      .then(snapshot => {
+        this.user = snapshot.data().name
+      })
+  },
   components: {
     NavigationBar
   }
